d254e404fdc277b361eeaa7137eec7581d7755dd
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-05-14 00:40:24 +0300

    Finish MinerPage data

--- src/services/homepage.ts
+++ src/services/homepage.ts
@@ -4,6 +4,7 @@ export const getHomepage = () => {
   const { genesisActors: actors, last24hActors, economics, head, miners } = database;
 
   return {
+    head,
     height: head.Height,
     circulatingSupply: (+actors.Supply / 1e18),
     burnt: (+actors.SupplyVM.FilBurnt / 1e18),
@@ -13,9 +14,11 @@ export const getHomepage = () => {
     last24hLocked: ((actors.SupplyVM.FilLocked - +last24hActors.SupplyVM.FilLocked) / 1e18),
     networkRaw: (+actors.Power.State.TotalBytesCommitted / 2 ** 50),
     last24hNewStorage: ((+actors.Power.State.TotalBytesCommitted - +last24hActors.Power.State.TotalBytesCommitted) / 2 ** 50),
+    totalBytesCommitted: +actors.Power.State.TotalBytesCommitted,
     activeMiners: actors.Power.State.MinerAboveMinPowerCount,
     sectorPledge: economics.sectorIp,
-    sector360daysReward: economics.sectorProjectedReward,
+    sectorProjectedReward: economics.sectorProjectedReward,
+    sectorProjectedReward1: economics.sectorProjectedReward1,
     sectorFaultFee: economics.sectorFaultFee,
     blockReward: (+actors.Reward.State.ThisEpochReward / 5 / 1e18),
     dayFilTiBReward: ((economics.sectorProjectedReward1 * 1024) / 32),
@@ -44,6 +47,11 @@ export const getHomepage = () => {
             sectorsCount: miners[miner].deadlines?.data.SectorsCount,
             activeCount: miners[miner].deadlines?.data.ActiveCount,
             faultsCount: miners[miner].deadlines?.data.FaultsCount,
+            nextDeadlines: miners[miner].deadlines?.data.nextDeadlines,
+            deadline: miners[miner].deadlines?.data.deadline,
+          },
+          preCommits: {
+            preCommitDeadlines: miners[miner].preCommits?.data?.PreCommitDeadlines,
           }
         }
       }
